b7a682c50d095b215084157df90c7f0a0ed611e8
Gun.io Whitespace Robot <contact@gun.io>
2011-10-31 09:35:35 -0400

    Remove whitespace [Gun.io WhitespaceBot]

--- WrAppDelegate.m
+++ WrAppDelegate.m
@@ -21,23 +21,23 @@
 	}
 }
 
-- (void)goToNormalWindow {	
+- (void)goToNormalWindow {
 	NSRect newFrame = [fullscreenWindow frameRectForContentRect:
                      [normalWindow contentRectForFrameRect:[normalWindow frame]]];
 	[fullscreenWindow
 	 setFrame:newFrame
 	 display:YES
 	 animate:YES];
-	
+
 	NSView* contentView = [[[fullscreenWindow contentView] retain] autorelease];
 	[fullscreenWindow setContentView:[[[NSView alloc] init] autorelease]];
-	
+
 	[normalWindow setContentView:contentView];
 	[normalWindow makeKeyAndOrderFront:nil];
-	
+
 	[fullscreenWindow close];
 	fullscreenWindow = nil;
-	
+
 	if ([[normalWindow screen] isEqual:[[NSScreen screens] objectAtIndex:0]]) {
 		[NSMenu setMenuBarVisible:YES];
 	}
@@ -50,26 +50,26 @@
 	if ([[normalWindow screen] isEqual:[[NSScreen screens] objectAtIndex:0]]) {
 		[NSMenu setMenuBarVisible:NO];
 	}
-	
+
 	[normalWindow orderOut:nil];
-	
+
 	fullscreenWindow = [[WrFullscreenWindow alloc]
                       initWithContentRect:[normalWindow contentRectForFrameRect:[normalWindow frame]]
                       styleMask:NSBorderlessWindowMask
                       backing:NSBackingStoreBuffered
                       defer:YES
                       screen:[normalWindow screen]];
-	
+
 	NSView *contentView = [[[normalWindow contentView] retain] autorelease];
 	[normalWindow setContentView:[[[NSView alloc] init] autorelease]];
-	
+
 	[fullscreenWindow setHidesOnDeactivate:YES];
 	[fullscreenWindow setLevel:NSFloatingWindowLevel];
 	[fullscreenWindow setContentView:contentView];
 	[fullscreenWindow setTitle:[normalWindow title]];
 	[fullscreenWindow makeKeyAndOrderFront:nil];
 	[fullscreenWindow setBackgroundColor:[NSColor whiteColor]];
-	
+
 	[fullscreenWindow
 	 setFrame:
 	 [fullscreenWindow
